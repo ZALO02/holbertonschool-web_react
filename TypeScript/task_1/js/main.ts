c083cb074ef4101307f1dd72ff80875197efb654
ZALOFARG <andre.work.go@gmail.com>
2024-03-22 01:57:03 -0500

    feat: class StudentClass created with all of its variables and methods

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -19,3 +19,31 @@ function printTeacher(firstName: string, lastName: string): string {
   const firstLetterFirstName = firstName.charAt(0).toUpperCase();
   return `${firstLetterFirstName}. ${lastName}`;
 }
+
+interface StudentConstructorArgs {
+  firstName: string,
+  lastName: string
+}
+
+interface Student {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements Student {
+  firstName: string;
+  lastName: string;
+
+  constructor(args: StudentConstructorArgs) {
+    this.firstName = args.firstName;
+    this.lastName = args.lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
